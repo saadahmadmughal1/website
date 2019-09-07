9b507e0bff5a1748c26155bab641e2b2b9cba643
Saad Ahmad <saadahmadmughal@hotmail.com>
2019-09-07 10:34:29 +0500

    Sep 19

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -100,15 +100,10 @@ jQuery(document).ready(function($) {
       data: str,
       success: function(msg) {
         // alert(msg);
-        if (msg == 'OK') {
-          $("#sendmessage").addClass("show");
+		 $("#sendmessage").addClass("show");
           $("#errormessage").removeClass("show");
           $('.contactForm').find("input, textarea").val("");
-        } else {
-          $("#sendmessage").removeClass("show");
-          $("#errormessage").addClass("show");
-          $('#errormessage').html(msg);
-        }
+
 
       }
     });
